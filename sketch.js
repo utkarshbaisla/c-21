310c14241645bc45c49e3000e4cf550502bcd89b
utkarshbaisla <87359258+utkarshbaisla@users.noreply.github.com>
2021-08-08 10:52:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+  bounceOff(movingRect,fixedRect)
+  if(isTouching(movingRect,fixedRect)) { 
+    fixedRect.shapeColor = "blue";
+     movingRect.shapeColor = "blue"; 
+    } else{ 
+      fixedRect.shapeColor = "green";
+   movingRect.shapeColor = "green";
+ }
+  drawSprites();
+}
+ 
